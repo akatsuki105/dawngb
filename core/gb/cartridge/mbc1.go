7b34f4952fd4a2e115d9a1150a2b0170e1b253f6
pokemium <bluejapan73+dev@gmail.com>
2024-01-09 01:01:30 +0400

    fix

--- core/gb/cartridge/mbc1.go
+++ core/gb/cartridge/mbc1.go
@@ -1,5 +1,7 @@
 package cartridge
 
+import . "github.com/akatsuki105/dugb/util/datasize"
+
 type mbc1 struct {
 	c          *Cartridge
 	ramEnabled bool
@@ -18,9 +20,10 @@ func newMBC1(c *Cartridge) mbc {
 func (m *mbc1) read(addr uint16) uint8 {
 	switch addr >> 12 {
 	case 0x0, 0x1, 0x2, 0x3:
-		return m.c.rom[addr]
+		return m.c.rom[addr&0x3FFF]
 	case 0x4, 0x5, 0x6, 0x7:
-		return m.c.rom[(uint32(m.romBank)<<14)|(uint32(addr&0x3FFF))]
+		bank := m.c.rom[(16*KB)*uint(m.romBank):]
+		return bank[addr&0x3FFF]
 	case 0xA, 0xB:
 		if m.ramEnabled {
 			return m.c.ram[(uint32(m.ramBank)<<13)|(uint32(addr&0x1FFF))]
@@ -32,19 +35,21 @@ func (m *mbc1) read(addr uint16) uint8 {
 func (m *mbc1) write(addr uint16, val uint8) {
 	switch addr >> 12 {
 	case 0x0, 0x1:
-		if val&0b1111 == 0x0A {
+		if val&0xF == 0x0A {
 			m.ramEnabled = true
 		} else {
 			m.ramEnabled = false
 		}
 	case 0x2, 0x3:
-		m.romBank = val & 0b11111
+		m.romBank &= 0b110_0000
+		m.romBank |= val & 0b11111
 		if m.romBank&0x1F == 0 {
-			m.romBank++
+			m.romBank |= 0x1
 		}
 	case 0x4, 0x5:
 		if m.mode == 0 {
-			m.romBank = (m.romBank & 0b11100000) | (val & 0b11111)
+			m.romBank &= 0x1F
+			m.romBank |= (val & 0b11) << 5
 		} else {
 			m.ramBank = val & 0b11
 		}
