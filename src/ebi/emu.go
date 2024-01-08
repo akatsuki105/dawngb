d43e8407997640851ba6ee2fa6c055d715748981
pokemium <bluejapan73+dev@gmail.com>
2024-01-08 14:39:53 +0400

    fix

--- src/ebi/emu.go
+++ src/ebi/emu.go
@@ -12,7 +12,7 @@ import (
 	"github.com/hajimehoshi/oto"
 )
 
-var music = true
+var music = false
 var samples = make([]byte, 4096)
 
 var keyMap = map[ebiten.Key]string{
@@ -24,6 +24,28 @@ var keyMap = map[ebiten.Key]string{
 	ebiten.KeyArrowDown:  "DOWN",
 	ebiten.KeyArrowLeft:  "LEFT",
 	ebiten.KeyArrowRight: "RIGHT",
+}
+
+var standardButtonToString = map[ebiten.StandardGamepadButton]string{
+	ebiten.StandardGamepadButtonRightTop:    "B",
+	ebiten.StandardGamepadButtonRightRight:  "A",
+	ebiten.StandardGamepadButtonCenterLeft:  "SELECT",
+	ebiten.StandardGamepadButtonCenterRight: "START",
+	ebiten.StandardGamepadButtonLeftBottom:  "DOWN",
+	ebiten.StandardGamepadButtonLeftRight:   "RIGHT",
+	ebiten.StandardGamepadButtonLeftLeft:    "LEFT",
+	ebiten.StandardGamepadButtonLeftTop:     "UP",
+}
+
+var inputMap = map[string]bool{
+	"A":      false,
+	"B":      false,
+	"START":  false,
+	"SELECT": false,
+	"UP":     false,
+	"DOWN":   false,
+	"LEFT":   false,
+	"RIGHT":  false,
 }
 
 type Emu struct {
@@ -132,7 +154,52 @@ func (e *Emu) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight
 }
 
 func (e *Emu) pollInput() {
+	for key := range inputMap {
+		inputMap[key] = false
+	}
+
+	e.pollKeyInput()
+	e.pollGamepadInput()
+
+	for key, input := range inputMap {
+		e.c.SetKeyInput(key, input)
+	}
+}
+
+func (e *Emu) pollKeyInput() {
 	for key, input := range keyMap {
-		e.c.SetKeyInput(input, ebiten.IsKeyPressed(key))
+		if _, ok := inputMap[input]; ok {
+			if ebiten.IsKeyPressed(key) {
+				inputMap[input] = true
+			}
+		}
+	}
+}
+
+func (e *Emu) pollGamepadInput() {
+	ids := ebiten.AppendGamepadIDs(nil)
+	for _, id := range ids {
+
+		for b, input := range standardButtonToString {
+			switch {
+			case ebiten.IsStandardGamepadButtonPressed(id, b):
+				if _, ok := inputMap[input]; ok {
+					inputMap[input] = true
+				}
+			}
+		}
+
+		switch ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisLeftStickHorizontal) {
+		case 1:
+			inputMap["RIGHT"] = true
+		case -1:
+			inputMap["LEFT"] = true
+		}
+		switch ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisLeftStickVertical) {
+		case 1:
+			inputMap["DOWN"] = true
+		case -1:
+			inputMap["UP"] = true
+		}
 	}
 }
