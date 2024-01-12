c99015b180d7adf93bfaf350d10009bbab9d36ae
pokemium <bluejapan73+dev@gmail.com>
2024-01-12 20:40:25 +0400

    fix

--- core/gb/timer.go
+++ core/gb/timer.go
@@ -8,12 +8,11 @@ import (
 var timaClock = [4]int64{64, 1, 4, 16}
 
 type timer struct {
-	g                   *GB
-	div, tima, tma, tac uint8
-	overflowEvent       sched.Event
+	g              *GB
+	tima, tma, tac uint8
 
 	updateEvent sched.Event
-	counter     int64
+	counter     int64 // 524288Hz(一番細かいのが524288Hzなのであとはそれの倍数で数えれば良い)
 }
 
 func newTimer(g *GB) *timer {
@@ -21,22 +20,23 @@ func newTimer(g *GB) *timer {
 		g: g,
 	}
 	t.updateEvent = *sched.NewEvent("GB_TIMER_UPDATE", t.update)
-	t.overflowEvent = *sched.NewEvent("GB_OVERFLOW", t.overflowTima)
 	return t
 }
 
 func (t *timer) Reset() {
-	t.div = 0x0
+	t.counter = 0
 	t.tima = 0
 	t.tma = 0
 	t.tac = 0
-	t.g.s.Schedule(&t.updateEvent, 16) // 524288Hz(一番細かいのが524288Hzなのであとはそれの倍数で数えれば良い)
+	t.g.s.Schedule(&t.updateEvent, 16)
 }
 
 func (t *timer) Read(addr uint16) uint8 {
+	x := t.g.cpu.Cycle / 4
 	switch addr {
 	case 0xFF04:
-		return t.div
+		div := t.counter / (16 * x)
+		return uint8(div)
 	case 0xFF05:
 		return t.tima
 	case 0xFF06:
@@ -50,7 +50,7 @@ func (t *timer) Read(addr uint16) uint8 {
 func (t *timer) Write(addr uint16, val uint8) {
 	switch addr {
 	case 0xFF04:
-		t.div = 0
+		t.counter = 0
 	case 0xFF05:
 		t.tima = val
 	case 0xFF06:
@@ -64,23 +64,15 @@ func (t *timer) update(cyclesLate int64) {
 	x := t.g.cpu.Cycle / 4
 
 	t.counter++
-	if (t.counter % (16 * x)) == 0 {
-		t.div++
-	}
-
 	if util.Bit(t.tac, 2) {
 		if (t.counter % (timaClock[t.tac&0b11] * x)) == 0 {
 			t.tima++
 			if t.tima == 0 {
-				t.g.s.Schedule(&t.overflowEvent, t.g.cpu.Cycle-cyclesLate)
+				t.tima = t.tma
+				t.g.requestInterrupt(2)
 			}
 		}
 	}
 
 	t.g.s.Schedule(&t.updateEvent, 16-cyclesLate) // 524288Hz
-}
-
-func (t *timer) overflowTima(cyclesLate int64) {
-	t.tima = t.tma
-	t.g.requestInterrupt(2)
 }
