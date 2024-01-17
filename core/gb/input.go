41605e29c10ae69bfe3d19353306f65f193c78b6
pokemium <bluejapan73+dev@gmail.com>
2024-01-18 04:32:25 +0900

    fix

--- core/gb/input.go
+++ core/gb/input.go
@@ -3,8 +3,9 @@ package gb
 import "github.com/akatsuki105/dugb/util"
 
 type Input struct {
-	g   *GB
-	val uint8
+	g        *GB
+	p14, p15 bool
+	joyp     uint8
 }
 
 func newInput(g *GB) *Input {
@@ -13,42 +14,46 @@ func newInput(g *GB) *Input {
 	}
 }
 
-func (i *Input) Reset(hasBIOS bool) {
-	i.val = 0x0F
-	if !hasBIOS {
-		i.val = 0xCF
+func (i *Input) Reset(_ bool) {
+	i.p14 = false
+	i.p15 = false
+}
+
+func (i *Input) poll() {
+	dpad := uint8(0x0)
+	dpad = util.SetBit(dpad, 0, !i.g.inputs[4+0])
+	dpad = util.SetBit(dpad, 1, !i.g.inputs[4+1])
+	dpad = util.SetBit(dpad, 2, !i.g.inputs[4+2])
+	dpad = util.SetBit(dpad, 3, !i.g.inputs[4+3])
+
+	button := uint8(0x0)
+	button = util.SetBit(button, 0, !i.g.inputs[0])
+	button = util.SetBit(button, 1, !i.g.inputs[1])
+	button = util.SetBit(button, 2, !i.g.inputs[2])
+	button = util.SetBit(button, 3, !i.g.inputs[3])
+
+	i.joyp = 0x0F
+	if !i.p14 {
+		i.joyp &= dpad
+	}
+	if !i.p15 {
+		i.joyp &= button
+	}
+
+	if i.joyp != 0x0F {
+		i.g.requestInterrupt(4)
 	}
 }
 
 func (i *Input) Read(addr uint16) uint8 {
-	pressed := false
-
-	val := i.val
-	if !util.Bit(val, 5) {
-		for key := 0; key < 4; key++ {
-			if i.g.inputs[key] {
-				val = util.SetBit(val, key, false)
-				pressed = true
-			}
-		}
-	}
-	if !util.Bit(val, 4) {
-		for key := 4; key < 8; key++ {
-			if i.g.inputs[key] {
-				val = util.SetBit(val, key-4, false)
-				pressed = true
-			}
-		}
-	}
-
-	if pressed {
-		i.g.requestInterrupt(4)
-	}
-
-	i.val = val
+	i.poll()
+	val := i.joyp
+	val = util.SetBit(val, 4, i.p14)
+	val = util.SetBit(val, 5, i.p15)
 	return val
 }
 
 func (i *Input) Write(addr uint16, val uint8) {
-	i.val = val | 0x0F
+	i.p14 = util.Bit(val, 4)
+	i.p15 = util.Bit(val, 5)
 }
