73aaee08c4a700621756b9899e01e396dd88267a
pokemium <bluejapan73+dev@gmail.com>
2024-01-18 19:46:42 +0900

    fix

--- core/gb/cartridge/mbc3.go
+++ core/gb/cartridge/mbc3.go
@@ -2,11 +2,16 @@ package cartridge
 
 import . "github.com/akatsuki105/dugb/util/datasize"
 
+type rtc struct {
+	latch uint8
+}
+
 type mbc3 struct {
 	c          *Cartridge
 	ramEnabled bool
 	romBank    uint8
-	ramBank    uint8
+	ramBank    uint
+	rtc        rtc
 }
 
 func newMBC3(c *Cartridge) mbc {
@@ -22,8 +27,12 @@ func (m *mbc3) read(addr uint16) uint8 {
 		return bank[addr&0x3FFF]
 	case 0xA, 0xB:
 		if m.ramEnabled {
-			bank := m.c.ram[(8*KB)*uint(m.ramBank):]
-			return bank[addr&0x1FFF]
+			if m.ramBank <= 0x03 {
+				return m.c.ram[(m.ramBank<<13)|uint(addr&0x1FFF)]
+			}
+
+			// RTC
+			return m.rtc.latch
 		}
 	}
 	return 0xFF
@@ -43,11 +52,14 @@ func (m *mbc3) write(addr uint16, val uint8) {
 			m.romBank |= 0x1
 		}
 	case 0x4, 0x5:
-		m.ramBank = val & 0b11
+		if val <= 0x0C {
+			m.ramBank = uint(val)
+		}
 	case 0xA, 0xB:
 		if m.ramEnabled {
-			bank := m.c.ram[(8*KB)*uint(m.ramBank):]
-			bank[addr&0x1FFF] = val
+			if m.ramBank <= 0x03 {
+				m.c.ram[(m.ramBank<<13)|uint(addr&0x1FFF)] = val
+			}
 		}
 	}
 }
