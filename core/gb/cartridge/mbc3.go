8210d8c9f6bd9e5ccde42d6adf0ab07f25e8d984
pokemium <bluejapan73+dev@gmail.com>
2024-01-18 19:53:28 +0900

    fix

--- core/gb/cartridge/mbc3.go
+++ core/gb/cartridge/mbc3.go
@@ -1,7 +1,5 @@
 package cartridge
 
-import . "github.com/akatsuki105/dugb/util/datasize"
-
 type rtc struct {
 	latch uint8
 }
@@ -9,7 +7,7 @@ type rtc struct {
 type mbc3 struct {
 	c          *Cartridge
 	ramEnabled bool
-	romBank    uint8
+	romBank    uint
 	ramBank    uint
 	rtc        rtc
 }
@@ -23,8 +21,7 @@ func (m *mbc3) read(addr uint16) uint8 {
 	case 0x0, 0x1, 0x2, 0x3:
 		return m.c.rom[addr&0x3FFF]
 	case 0x4, 0x5, 0x6, 0x7:
-		bank := m.c.rom[(16*KB)*uint(m.romBank):]
-		return bank[addr&0x3FFF]
+		return m.c.rom[(m.romBank<<14)|uint(addr&0x3FFF)]
 	case 0xA, 0xB:
 		if m.ramEnabled {
 			if m.ramBank <= 0x03 {
@@ -47,7 +44,7 @@ func (m *mbc3) write(addr uint16, val uint8) {
 			m.ramEnabled = false
 		}
 	case 0x2, 0x3:
-		m.romBank = val & 0b111_1111
+		m.romBank = uint(val & 0b111_1111)
 		if m.romBank&0x1F == 0 {
 			m.romBank |= 0x1
 		}
