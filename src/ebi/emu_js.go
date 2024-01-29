fe269a436f83fea936906e1b3fa3ac032766ea78
pokemium <bluejapan73+dev@gmail.com>
2024-01-29 17:31:52 +0900

    wip

--- src/ebi/emu_js.go
+++ src/ebi/emu_js.go
@@ -1,12 +1,16 @@
 package main
 
-import "syscall/js"
+import (
+	"fmt"
+	"syscall/js"
+)
 
 func init() {
 	js.Global().Set("press", js.FuncOf(press))
 }
 
 func press(this js.Value, args []js.Value) any {
+	fmt.Println("press", args[0].String())
 	inputMap[args[0].String()] = true
 	return nil
 }
