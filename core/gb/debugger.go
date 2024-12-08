60e1413689ae9ec25d2fb09af2bdbcd21ee094dc
akatsuki105 <bluejapan73+dev@gmail.com>
2024-12-09 08:04:23 +0900

    wip

--- core/gb/debugger.go
+++ core/gb/debugger.go
@@ -13,7 +13,17 @@ func (g *GB) GetValue(which uint64) uint64 {
 }
 
 func (g *GB) ViewMemory(memID int, addr uint32, width int, flag uint32) uint32 {
-	return 0
+	addr16 := uint16(addr)
+	switch width {
+	case 1:
+		return uint32(g.cpu.Read(addr16))
+	case 2:
+		return uint32(g.cpu.Read(addr16)) | (uint32(g.cpu.Read(addr16+1)) << 8)
+	case 4:
+		return uint32(g.cpu.Read(addr16)) | (uint32(g.cpu.Read(addr16+1)) << 8) | (uint32(g.cpu.Read(addr16+2)) << 16) | (uint32(g.cpu.Read(addr16+3)) << 24)
+	default:
+		return 0
+	}
 }
 
 func (g *GB) GetChunk(chunkID uint8) []uint8 {
